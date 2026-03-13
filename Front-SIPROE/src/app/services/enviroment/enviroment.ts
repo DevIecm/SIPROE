f38045f76a112ef07c12393a8987bbf60cc64e03
Luis Enrique Pérez Rendón <enrique.perez@iecm.mx>
2026-03-13 17:16:02 -0600

    Se actualiza calendario

--- Front-SIPROE/src/app/services/enviroment/enviroment.ts
+++ Front-SIPROE/src/app/services/enviroment/enviroment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   //apiUrl: 'http://localhost:4000/api/'
   //apiUrl: 'https://app.iecm.mx/siproe-aleatorio2025/api/'
-  apiUrl: 'http://145.0.40.23:4000/api/'
+  apiUrl: 'https://aplicaciones2.iecm.mx/siproe-aleatorio-2026-2027/api/'
 }; 
